6ec6134432c0393dd033e9a844febbb8672f4542
MohammedHassanSalat <mohh20033@gmail.com>
2025-02-12 06:23:22 +0200

    reset password form initial design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ForgetPasseordComponent } from './components/forget-passeord/forget-passeord.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: SignupComponent },
   { path: 'forgetpassword', component: ForgetPasseordComponent },
+  { path: 'resetpassword', component: ResetPasswordComponent },
 ];
