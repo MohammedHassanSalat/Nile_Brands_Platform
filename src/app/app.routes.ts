6f8f5c2771bed3578d852722f9744733abb17a42
MohammedHassanSalat <mohh20033@gmail.com>
2025-02-12 07:35:42 +0200

    login form initial design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignupComponent } from './components/signup/signup.component';
 import { HomeComponent } from './components/home/home.component';
 import { ForgetPasseordComponent } from './components/forget-passeord/forget-passeord.component';
 import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'register', component: SignupComponent },
   { path: 'forgetpassword', component: ForgetPasseordComponent },
   { path: 'resetpassword', component: ResetPasswordComponent },
+  { path: 'signin', component: LoginComponent },
 ];
