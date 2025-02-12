3b9f4d6af1e6c0f1191aa994c68b11bc178cb5c2
MohammedHassanSalat <mohh20033@gmail.com>
2025-02-12 05:39:24 +0200

    update tailwind config & signup form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        'primary': "#033555",
+        'secondary': "#828282",
+        'fontColor': "#242526",
+      },
       animation: {
-        'fade-in': 'fadeIn 1s ease-out forwards',
-        'zoom-in': 'zoomIn 1.5s ease-out forwards',
-        'slide-up': 'slideUp 1s ease-out forwards',
-        'bounce-in': 'bounceIn 1s ease-out forwards',
+        "fade-in": "fadeIn 1s ease-out forwards",
+        "zoom-in": "zoomIn 1.5s ease-out forwards",
+        "slide-up": "slideUp 1s ease-out forwards",
+        "bounce-in": "bounceIn 1s ease-out forwards",
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' }
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
         },
         zoomIn: {
-          '0%': { opacity: '0', transform: 'scale(1.1)' },
-          '100%': { opacity: '1', transform: 'scale(1)' }
+          "0%": { opacity: "0", transform: "scale(1.1)" },
+          "100%": { opacity: "1", transform: "scale(1)" },
         },
         slideUp: {
-          '0%': { opacity: '0', transform: 'translateY(30px)' },
-          '100%': { opacity: '1', transform: 'translateY(0)' }
+          "0%": { opacity: "0", transform: "translateY(30px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
         },
         bounceIn: {
-          '0%': { opacity: '0', transform: 'scale(0.8)' },
-          '50%': { opacity: '1', transform: 'scale(1.1)' },
-          '100%': { transform: 'scale(1)' }
-        }
-      }
-    }
+          "0%": { opacity: "0", transform: "scale(0.8)" },
+          "50%": { opacity: "1", transform: "scale(1.1)" },
+          "100%": { transform: "scale(1)" },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
